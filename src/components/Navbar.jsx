7e7f6756a8826c1179912cc51740de50b455b50f
Pratik <pratiknimbole27@gmail.com>
2026-04-03 22:27:58 +0530

    global context

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,30 @@
 import { AppBar, Toolbar, Typography, Select, MenuItem, Box } from "@mui/material";
+import { useAppContext } from "../context/AppContext";
 
 function Navbar() {
+  const { role, setRole } = useAppContext();
+
   return (
     <AppBar position="static">
       <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
         
         <Typography variant="h6">
-          Financial Dashboard
+          Transaction Dashboard
         </Typography>
 
         <Box>
-          <Typography variant="body2" sx={{ marginRight: 1, display: "inline" }}>
+          <Typography variant="body2" sx={{ mr: 1, display: "inline" }}>
             Role:
           </Typography>
 
-          <Select size="small" defaultValue="admin" sx={{ background: "white" }}>
+          <Select
+            size="small"
+            value={role}
+            onChange={(e) => setRole(e.target.value)}
+            sx={{ background: "white" }}
+          >
             <MenuItem value="admin">Admin</MenuItem>
-            <MenuItem value="user">User</MenuItem>
+            <MenuItem value="viewer">Viewer</MenuItem>
           </Select>
         </Box>
 
